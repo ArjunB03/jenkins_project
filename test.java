1fcd5f6c6e8422501a2f8536f52caad435ce8e92
arjun bharath <arjunbharath2003@gmail.com>
2024-11-20 09:47:05 +0530

    adding files to repository

---
+++ test.java
@@ -0,0 +1,17 @@
+// Simple Java Program
+public class Main {
+    public static void main(String[] args) {
+        // Print a greeting
+        System.out.println("Hello, World!");
+
+        // Declare two numbers
+        int number1 = 10;
+        int number2 = 20;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
